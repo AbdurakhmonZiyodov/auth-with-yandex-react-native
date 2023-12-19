ac9e55a4805a7a6d325592b509d67147b73960d9
Abdurakhmon <abdurakhmonziyodov@gmail.com>
2023-12-19 12:43:09 +0500

    first commit

---
+++ ios/CoolWebViewManager.m
@@ -0,0 +1,8 @@
+//
+//  CoolWebViewManager.m
+//  AuthWithYandexExample
+//
+//  Created by Abdurahmon Ziyodov on 19/12/23.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/CoolWebViewManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoolWebViewManager.swift
+//  AuthWithYandexExample
+//
+//  Created by Abdurahmon Ziyodov on 19/12/23.
+//
+
+import Foundation
